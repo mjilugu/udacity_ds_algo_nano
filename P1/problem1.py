21793b6b63990762ed81df0c023631bc0bf1f3d9
Moses Jilugu <mjilugu@gmail.com>
2021-10-27 08:08:36 +0100

    Project-2: Show me the data structures

--- P1/problem1.py
+++ P1/problem1.py
@@ -34,19 +34,32 @@ class LRU_Cache(object):
         return curr
 
 if __name__ == "__main__":
+    print(f"\n*** our_cache initiated with capacity 5 ***\n")
     our_cache = LRU_Cache(5)
+    print(f"Test0: our_cache.get(1000) returns -1, cache is empty")
+    #-1
+    print(f"{our_cache.get(0)}")
 
+    print(f"\n*** Add four values to our_cache ***\n")
     our_cache.set(1, 1);
     our_cache.set(2, 2);
     our_cache.set(3, 3);
     our_cache.set(4, 4);
 
+    print(f"Test1: our_cache.get(1) returns 1")
+    #1
+    print(f"{our_cache.get(1)}")
+    print(f"Test2: our_cache.get(2) returns 2")
+    #2
+    print(f"{our_cache.get(2)}")
+    print(f"Test3: our_cache.get(9) returns -1")
+    #-1
+    print(f"{our_cache.get(9)}")
 
-    assert our_cache.get(1) == 1, "our_cache.get(1) should return 1"       # returns 1
-    assert our_cache.get(2) == 2, "Fail"      # returns 2
-    assert our_cache.get(9) == -1, "Fail"     # returns -1 because 9 is not present in the cache
-
+    print(f"\n*** Add two new elements to our_cache ***\n")
     our_cache.set(5, 5) 
     our_cache.set(6, 6)
 
-    assert our_cache.get(3) == -1, "Fail"      # returns -1 because the cache reached it's capacity and 3 was the least recently used entry
+    print(f"Test3: our_cache.get(3) returns -1 as 3 was replaced as least recently used element")
+    #-1
+    print(f"{our_cache.get(3)}")
