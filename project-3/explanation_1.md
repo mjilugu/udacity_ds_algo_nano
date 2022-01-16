d0d44e4af2d164e4fc63e49d0ad401f769a70b8f
Moses Jilugu <mjilugu@gmail.com>
2022-01-16 15:22:07 +0000

    Project-3: Problems vs Algorithms

--- project-3/explanation_1.md
+++ project-3/explanation_1.md
@@ -1,36 +1,36 @@
-# Problem 1 - Square Root
+# Problem 1 - Square Root of an Integer
 
 Square root implementation returning the square root of a number or the floor int of the square root.
 The implementation checks the integers between 0 and number//2. Using divide and conquer we compare the square value of the mid value with the number and reduce the check to the half of the number where the square root lies.
 Time complexity is O(logn) and space is constant O(1).
 
-# Problem 2 - Rotated Array Search
+# Problem 2 - Search in a Rotated Sorted Array
 
 Function rotated_array_search finds given number in a rotated array. Splitting the array in two, one portion will be sorted and we can detect if the number lays in that portion. This helps us eliminate one half in every iteration. 
 Time complexity is O(logn) and space is constant O(1) since we search in place.
 
-# Problem 3 - Rearrange Array Digit To Get Max Sum Pair
+# Problem 3 - Rearrange Array Elements
 
 The solution involves first sorting the array in descending order. We then remove two elements at a time from the array untill it is empty appending the first as next lower digit of num_1 and appending the second as next lower digit of num_2. 
 Time complexity is O(nlogn) from the quick sort and space is constant O(1).
 
-# Problem 4 - Sort Array of 0s, 1s and 2s
+# Problem 4 - Dutch National Flag Problem
 
 General approach is to move all the 0s to the front and all the 2s to the back. The 1s will be in correct position after that. 
 Time complexity is O(n) and space is constant O(1) as we sort in place.
 
-# Problem 5 - Trie
+# Problem 5 - Autocomplete with Tries
 
 The trie has the usual insert() and find() methods that add/locate nodes. For a given node they iterate on the characters to either insert the word or locate it. Both have time efficiency of O(n) and space efficiency of O(1).
 The suffixes() method takes a prefix and it first locates the node matching the last characher in the prefix. If any char in the prefix does not have a matching node we return empty list of suffixes. Otherwise we call _suffixes_rec() with the last node in prefix and empty suffix. The _suffixes_rec() method searches recursively the sub-tree starting at prefix as root and collects all nodes which are wordends and the words they form from the root (prefix).
 This list is returned as the suffix list. The time efficiency of this is linear O(n) as we the whole sub tree and space complexity is O(1) 
 
-# Problem 6 - Min Max
+# Problem 6 - Max and Min in a Unsorted Array
 
 Initializing both min and max to the first element, we traverse the list comparing every element to min and max and assign where suitable.
 The complexity is O(n) and space is constant O(1).
 
-# Problem 7 - Web Router
+# Problem 7 - HTTPRouter using a Trie
 
 The main methods in the implementation are add_handler() and lookup(). 
 add_handler() splits the path into array of components and passes the array and handler to the trie insert() method. The router trie insert() passes these to the root node insert which recursively calls insert to add every path in the array. The the handler gets passed to the last node. Efficiency of insert is linear O(n) and space efficiency is constant O(1).
---
+++ project-3/problem_5.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Building a Trie in Python
+# 
+# Before we start let us reiterate the key components of a Trie or Prefix Tree. A trie is a tree-like data structure that stores a dynamic set of strings. Tries are commonly used to facilitate operations like predictive text or autocomplete features on mobile phones or web search.
+# 
+# Before we move into the autocomplete function we need to create a working trie for storing strings.  We will create two classes:
+# * A `Trie` class that contains the root node (empty string)
+# * A `TrieNode` class that exposes the general functionality of the Trie, like inserting a word or finding the node which represents a prefix.
+# 
+# Give it a try by implementing the `TrieNode` and `Trie` classes below!
+
+# In[1]:
+
+
+## Represents a single node in the Trie
+class TrieNode:
+    def __init__(self):
+        ## Initialize this node in the Trie
+        self.children = {}
+        self.word_end = False
+    
+    def insert(self, char):
+        ## Add a child node in this Trie
+        if char not in self.children:
+            self.children[char] = TrieNode()
+        
+## The Trie itself containing the root node and insert/find functions
+class Trie:
+    def __init__(self):
+        ## Initialize this Trie (add a root node)
+        self.root = TrieNode()
+
+    def insert(self, word):
+        ## Add a word to the Trie
+        curr = self.root
+        
+        for char in word:
+            curr.insert(char)
+            curr = curr.children[char]
+            
+        curr.word_end = True
+
+    def find(self, prefix):
+        ## Find the Trie node that represents this prefix
+        
+        curr = self.root
+        
+        for char in prefix:
+            if char not in curr.children:
+                return None
+            curr = curr.children[char]
+            
+        return curr
+
+
+# # Finding Suffixes
+# 
+# Now that we have a functioning Trie, we need to add the ability to list suffixes to implement our autocomplete feature.  To do that, we need to implement a new function on the `TrieNode` object that will return all complete word suffixes that exist below it in the trie.  For example, if our Trie contains the words `["fun", "function", "factory"]` and we ask for suffixes from the `f` node, we would expect to receive `["un", "unction", "actory"]` back from `node.suffixes()`.
+# 
+# Using the code you wrote for the `TrieNode` above, try to add the suffixes function below. (Hint: recurse down the trie, collecting suffixes as you go.)
+
+# In[2]:
+
+
+class TrieNode:
+    def __init__(self):
+        ## Initialize this node in the Trie
+        self.children = {}
+        self.word_end = False
+    
+    def insert(self, char):
+        ## Add a child node in this Trie
+        if char not in self.children:
+            self.children[char] = TrieNode()
+        
+    def suffixes(self, suffix = ''):
+        ## Recursive function that collects the suffix for 
+        ## all complete words below this point
+        
+        prefix_node = self
+        
+        for char in suffix:
+            if char not in prefix_node.children:
+                return []
+            prefix_node = prefix_node.children[char]
+            
+        return self._suffixes_rec(prefix_node)
+    
+    def _suffixes_rec(self, prefix_node, suffix = ''):
+        
+        suffixes = []
+        if len(suffix) > 0 and prefix_node.word_end:
+            suffixes.append(f"'{suffix}'")
+        
+        for key,value in prefix_node.children.items():
+            suffixes.extend(self._suffixes_rec(value, suffix + key))
+            
+        return suffixes
+
+
+# # Testing it all out
+# 
+# Run the following code to add some words to your trie and then use the interactive search box to see what your code returns.
+
+# In[3]:
+
+
+MyTrie = Trie()
+wordList = [
+    "ant", "anthology", "antagonist", "antonym", 
+    "fun", "function", "factory", 
+    "trie", "trigger", "trigonometry", "tripod"
+]
+for word in wordList:
+    MyTrie.insert(word)
+
+
+# In[4]:
+
+
+from ipywidgets import widgets
+from IPython.display import display
+from ipywidgets import interact
+def f(prefix):
+    if prefix != '':
+        prefixNode = MyTrie.find(prefix)
+        if prefixNode:
+            print('\n'.join(prefixNode.suffixes()))
+        else:
+            print(prefix + " not found")
+    else:
+        print('')
+interact(f,prefix='');
+
